16e753ebc15c74af76af544338003dae304b1152
Solomon Great <greatsoma2019@gmail.com>
2025-09-17 09:30:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Product from "./components/Products";
+import MiniHero from "./components/MiniHero";
+import { Metadata } from "next";
+
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <About />
+      <MiniHero />
       <Services />
       <Product />
       <Contact />
