3ebfc651d1b1f11c40765ca6606316a22b476b0b
Solomon Great <greatsoma2019@gmail.com>
2025-09-17 09:40:45 +0100

    Add MiniHero component with styles
    Introduces the MiniHero React component and its associated SCSS module for layout and styling. The component displays a headline, description, button, and two images in a responsive grid.

---
+++ app/components/MiniHero.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import styles from "./MiniHero.module.scss"
+
+const MiniHero = () => {
+  return (
+    <div className={styles.miniHero}>
+        <div className="container">
+            <div className={styles.miniHeroContent}>
+
+            <div className={styles.textArea}>
+                <h2>
+                Innovative Technology solutions
+                </h2>
+                <p>
+                Empowering businesses through innovative technology solutions that drive growth and success in the digital age.
+                </p>
+                <button>
+                Explore
+                </button>
+            </div>
+             <Image src="/about-image-1.jpg" alt="About Techistry 0ne" width={300} height={400} />
+             <Image src="/about-image-2.jpg" alt="About Techistry two" width={300} height={400} />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MiniHero
