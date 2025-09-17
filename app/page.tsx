9665d36c8435f330c2e6f10b8abaa51cba687ed3
Solomon Great <greatsoma2019@gmail.com>
2025-09-17 11:34:01 +0100

    Remove Header and Footer from Home page
    The Home page no longer includes the Header and Footer components. This change streamlines the page layout, possibly to delegate these components to a higher-level layout or wrapper.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import Product from "./components/Products";
 import MiniHero from "./components/MiniHero";
-import { Metadata } from "next";
 
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <MiniHero />
       <Services />
       <Product />
       <Contact />
-      <Footer />
     </>
   );
 }
