e7690273833c88e90e41a227198f3f4c6b1a4777
Solomon Great <greatsoma2019@gmail.com>
2025-09-17 11:45:46 +0100

    Update layout with metadata, AOS, and components
    Enhanced the RootLayout by updating metadata, adding keywords and icons, and integrating AOS animation support. Header and Footer components are now included, and font link formatting was improved for better readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,21 @@
-import type { Metadata } from "next";
+/* eslint-disable @next/next/no-page-custom-font */
+import { Metadata } from "next";
 import "./globals.scss";
+import "aos/dist/aos.css";
+import AOSInitializer from "./AosInitializer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Techistry Integrated - Technology Solutions",
+  title: "Techistry Integrated",
   description:
-    "Leading technology company providing innovative solutions for modern businesses",
+    "Welcome to Techistry, your go-to solution for integrated technology services.",
+  keywords: "Techistry, technology, services, integrated solutions",
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -15,11 +26,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
-<link href="https://fonts.googleapis.com/css2?family=Krub:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Unbounded:wght@200..900&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Krub:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Unbounded:wght@200..900&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body>{children}</body>
+      <AOSInitializer />
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
